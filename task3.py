e9f60c4a7f9c33fc73018d2204bc0a17a58a90b5
aispace-code <aisuluu998@gmail.com>
2019-11-13 17:25:13 +0600

    «Aisuluu»

---
+++ task3.py
@@ -0,0 +1,3 @@
+a = int(input('Give us any number '))
+print('The next number for the number ' + str(a) + ' is ' + str(a + 1))
+print('The previous number for the number ' + str(a) + ' is ' + str(a - 1))
